13a43bead0d57f4772c0ddaf683dcff080494739
Andrey <andreymdm@gmail.com>
2024-02-15 03:22:17 +0300

    lesson3_1

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,12 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString(string) {
+    let newArr = string.filter((item) => {
+        if (item === "черный" || item === "красный" || item === "желтый") {
+            return true
+        }
+    }).join('-')
+    return newArr
+}
+
+console.log(createColorString(colors));
