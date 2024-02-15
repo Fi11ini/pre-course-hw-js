e30aeb118dc0b2fb23d6bdf0000e51548cffc21c
Andrey <andreymdm@gmail.com>
2024-02-15 03:52:52 +0300

    lesson3_2 try two

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,12 +1,12 @@
 const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
 
+
 let getOrange = function (fructs) {
-    if (fructs.indexOf("апельсин")) {
+    if (fructs.indexOf("апельсин") !== -1) {
         return "Ура! нашел"
     } else {
-        return "Придется поискать в другом магазине…"
+        return 'Придется поискать в другом магазине…'
     }
-
 }
 
 console.log(getOrange(words))
